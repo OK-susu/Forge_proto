98bcbf0c4e41e429d33e3d400b52531dd9070350
InJae <108115109+OK-susu@users.noreply.github.com>
2022-12-06 18:43:58 +0900

    Enc, Dec 추가

--- myForge.js
+++ myForge.js
@@ -4,12 +4,13 @@ const crypto = require("crypto");
 
 function _forge(){};
 
+let cert_pem = __dirname + '\\cert\\cert.pem';
+let privateKey_pem = __dirname + '\\cert\\privateKey.pem';
+let publicKey_pem = __dirname + '\\cert\\publicKey.pem';
+let path = "./cert";
+
 _forge.prototype.CreatePem = function() {    
-
-    let cert_pem = __dirname + '\\cert\\cert.pem';
-    let privateKey_pem = __dirname + '\\cert\\privateKey.pem';
-
-    let path = "./cert";
+    
     const isExists = fs.existsSync( path );
     if( !isExists ) {
         fs.mkdirSync( path, { recursive: true } );
@@ -18,7 +19,7 @@ _forge.prototype.CreatePem = function() {
         let cert_fileExists = fs.existsSync(cert_pem);
         let privateKey_fileExists = fs.existsSync(privateKey_pem);
 
-         if ((!cert_fileExists) || (!privateKey_fileExists)) {
+        if ((!cert_fileExists) || (!privateKey_fileExists)) {
             const keys = forge.pki.rsa.generateKeyPair(2048);
             const cert = forge.pki.createCertificate();
             
@@ -43,11 +44,19 @@ _forge.prototype.CreatePem = function() {
             cert.sign(keys.privateKey);
                     
             const privateKey = forge.pki.privateKeyToPem(keys.privateKey);
+            const publicKey = forge.pki.publicKeyToPem(keys.publicKey);
             const pem = forge.pki.certificateToPem(cert);
 
             fs.writeFileSync(cert_pem, pem, '', function (err) {
                 if (err) {
                     console.log("[X]Create cert File Error:" + err);
+                    return false;
+                }                
+            });
+
+            fs.writeFileSync(publicKey_pem, publicKey, '', function (err) {
+                if (err) {
+                    console.log("[X]Create publicKey File Error:" + err);
                     return false;
                 }                
             });
@@ -65,10 +74,48 @@ _forge.prototype.CreatePem = function() {
         }
 
     } catch (e) {
-        consoel.log("[X]Except: " + e.message);
+        console.log("[X]Except: " + e.message);
         return false;
     }        
     return true;
 };
+
+_forge.prototype.RsaEnc = function (plainText) {
+    let Result_Enc = "";
+    const isExists = fs.existsSync(path);
+    if (!isExists) {
+        console.log("[X]No Exists Cert Path");
+        console.log("[X]First, try CreatePem function");
+        return Result_Enc;
+    }
+    try {
+        let publicKey = forge.pki.publicKeyFromPem(fs.readFileSync(publicKey_pem, 'utf8'));
+        Result_Enc = publicKey.encrypt(forge.util.encodeUtf8(plainText));
+        return Result_Enc;
+
+    } catch (e) {
+        console.log("[X]Except: " + e.message);
+        return Result_Enc;
+    }
+}
+
+_forge.prototype.RsaDec = function (CyperText) {
+    let Result_Dec = "";
+    const isExists = fs.existsSync(path);
+    if (!isExists) {
+        console.log("[X]No Exists Cert Path");
+        console.log("[X]First, try CreatePem function");
+        return Result_Dec;
+    }
+    try {
+        let privateKey = forge.pki.privateKeyFromPem(fs.readFileSync(privateKey_pem, 'utf8'));
+        Result_Dec = forge.util.decodeUtf8(privateKey.decrypt(CyperText));
+        return Result_Dec;
+
+    } catch (e) {
+        console.log("[X]Except: " + e.message);
+        return Result_Dec;
+    }
+}
 
 module.exports = _forge;
